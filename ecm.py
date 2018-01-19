b7adcf77f14eec78cbb6ce03c35fff00fb90e49f
Adam Buran <aburan28@gmail.com>
2018-01-19 03:35:03 -0800

    Create ecm.py

---
+++ ecm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+class FiniteField:
+
+class AffinePlane
+class ProjectivePlane:
+class EllipticCurve(object):
+    
+    def __init__(
+    
+    
+class ECM(object):
+    def __init__(self):
+        self.bound_one =
+    
